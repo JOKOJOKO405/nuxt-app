08bf0270d832b44fbb3e145bd960f746b1047108
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-30 11:54:08 +0900

    moment追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,8 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    ['@nuxtjs/moment', ['ja']]
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
