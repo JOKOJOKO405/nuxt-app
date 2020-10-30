d7889fb28feed7c3d8e1591a992e43e84fe21945
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-30 10:58:44 +0900

    storeのaction設定

---
+++ store/todolist.js
@@ -0,0 +1,49 @@
+import firebase from '@/plugins/firebase'
+import { firestoreAction } from 'vuexfire'
+
+// データベース
+const db = firebase.firestore();
+const todoRef = db.collection('todos')
+
+export const state = function(){
+  todos: []
+}
+
+export const actions = {
+  // 初期化
+  init: firestoreAction(({ bindFirestoreRef }) => {
+    bindFirestoreRef('todos', todoRef)
+  }),
+  add: firestoreAction((action, task, limit='') => {
+    if(task.trim().length){
+      todoRef.add({
+        task: task,
+        done: false,
+        date: firebase.firestore.FieldValue.serverTimestamp(),
+        modify: false,
+        limit: limit
+      })
+    }
+  }),
+  delete: firestoreAction((action, id) =>{
+    todoRef.doc(id).delete();
+  }),
+  toggled: firestoreAction((action, todo) => {
+    todoRef.doc(todo.id).update({
+      done: !todo.done
+    })
+  }),
+  modify: firestoreAction((action, id, newTask, newLimit) => {
+    todoRef.doc(id).update({
+      task: newTask,
+      modify: false,
+      limit: newLimit,
+    })
+  }),
+}
+
+export const getter = {
+  oerderedDate: state => {
+    return _.sortBy(state.todos, 'date')
+  }
+}
