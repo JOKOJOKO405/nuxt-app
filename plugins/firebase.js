16203c2f13ba5193a9b1f9ebfa829e69aaa5950f
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-30 10:25:05 +0900

    コメント付与

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,9 +1,11 @@
 import firebase from 'firebase'
 
+// .envからfirebaseのIDを取得
 const config = {
   projectId: process.env.FIREBASE_PROJECT_ID
 }
 
+// 初期化作業
 if(!firebase.apps.length){
   firebase.initializeApp(config)
 }
