c0e24502e884170e19d38d29df4942d2963c301a
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-01 06:04:53 +0900

    フォーマット成形

--- components/FormComponent.vue
+++ components/FormComponent.vue
@@ -1,39 +1,38 @@
 <template>
-  <div>
-    <form @submit.prevent="addTodo">
-        <input type="text" v-model="input">
-        <input type="datetime-local" v-model="limit">
-        <!-- <button>add</button> -->
-        <v-app>
-          <v-btn
-  color="accent"
-  elevation="2"
-  rounded
-  x-large
-  @click.prevent="addTodo"
->ADD TODO</v-btn>
-        </v-app>
-      </form>
-  </div>
+  <form @submit.prevent="addTodo">
+    <input type="text" v-model="input" />
+    <input type="datetime-local" v-model="limit" />
+    <!-- <button>add</button> -->
+    <v-app>
+      <v-btn
+        color="accent"
+        elevation="2"
+        rounded
+        x-large
+        @click.prevent="addTodo"
+        >ADD TODO</v-btn
+      >
+    </v-app>
+  </form>
 </template>
 
 <script>
 export default {
-  data: function(){
+  data: function () {
     return {
-      input: '',
-      limit: '',
-      index: '',
-      newInput: '',
-      newLimit: '',
-    }
+      input: "",
+      limit: "",
+      index: "",
+      newInput: "",
+      newLimit: "",
+    };
   },
   methods: {
-    addTodo: function(){
-      this.$store.dispatch('todolist/add', [this.input, this.limit]);
-      this.input = ''
-      this.limit = ''
+    addTodo: function () {
+      this.$store.dispatch("todolist/add", [this.input, this.limit]);
+      this.input = "";
+      this.limit = "";
     },
   },
-}
+};
 </script>
