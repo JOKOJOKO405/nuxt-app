0c1a01aa3bdaf5ab6a0860e569d9ddfba83a9717
JOKOJOKO405 <biz.kj405@gmail.com>
2020-11-01 06:39:00 +0900

    表示調整

--- components/ListComponent.vue
+++ components/ListComponent.vue
@@ -1,68 +1,79 @@
 <template>
   <div>
     <h1>MY TODO</h1>
-      <ul>
-        <li v-for="todo in todos" :key="todo.id">
-          <div v-if="todo.date">
-            <p :class="{ done: todo.done }">
-            <input type="checkbox" @change="toggled(todo)" :checked="todo.done" id=""> 
-            </p>
-            <p @click="modifyShow(todo)">
-              {{ todo.task }}
-              {{ todo.date.toDate() | dateFilter }}
-            </p>
-            <p v-if="todo.modify">
-              <input type="text" v-model="newInput">
-              <input type="datetime-local" v-model="newLimit">
-              <button @click.prevent="modify(todo.id)">modify</button>
-            </p>
-            {{ todo.limit | dateFilter }}
-          <button @click.prevent="deleteTodo(todo.id)">del</button>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <div v-if="todo.date">
+          <div :class="{ done: todo.done }">
+            <div class="flex">
+              <v-checkbox
+                :checked="todo.done"
+                @change="toggled(todo)"
+                hide-details
+              ></v-checkbox>
+
+              <p @click="modifyShow(todo)">
+                {{ todo.task }}
+                {{ todo.date.toDate() | dateFilter }}
+              </p>
+            </div>
           </div>
-        </li>
-      </ul>
+          <p v-if="todo.modify">
+            <input type="text" v-model="newInput" />
+            <input type="datetime-local" v-model="newLimit" />
+            <button @click.prevent="modify(todo.id)">modify</button>
+          </p>
+          {{ todo.limit | dateFilter }}
+          <button @click.prevent="deleteTodo(todo.id)">del</button>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import moment from 'moment';
+import moment from "moment";
 export default {
-  data: function(){
+  data: function () {
     return {
-      input: '',
-      limit: '',
-      index: '',
-      newInput: '',
-      newLimit: '',
-    }
+      input: "",
+      limit: "",
+      index: "",
+      newInput: "",
+      newLimit: "",
+    };
   },
   methods: {
-    deleteTodo: function(id){
-      this.$store.dispatch('todolist/delete', id)
+    deleteTodo: function (id) {
+      this.$store.dispatch("todolist/delete", id);
     },
-    toggled: function(todo){
-      this.$store.dispatch('todolist/toggled', todo)
+    toggled: function (todo) {
+      this.$store.dispatch("todolist/toggled", todo);
     },
-    modify: function(id){
-      this.$store.dispatch('todolist/modify', [id, this.newInput, this.newLimit])
-      this.newInput = '';
+    modify: function (id) {
+      this.$store.dispatch("todolist/modify", [
+        id,
+        this.newInput,
+        this.newLimit,
+      ]);
+      this.newInput = "";
     },
-    modifyShow: function(todo) {
-      this.$store.dispatch('todolist/show', todo)
+    modifyShow: function (todo) {
+      this.$store.dispatch("todolist/show", todo);
     },
-    allCheck: function(todo) {
-      this.$store.dispatch('todolist/allCheck', todo)
-    }
+    // allCheck: function(todo) {
+    //   this.$store.dispatch('todolist/allCheck', todo)
+    // }
   },
   computed: {
-    todos(){
-      return this.$store.getters['todolist/orderedDate']
+    todos() {
+      return this.$store.getters["todolist/orderedDate"];
     },
   },
   filters: {
-    dateFilter: function(date){
-      return moment(date).format('YYYY.MM.DD HH:mm:ss')
+    dateFilter: function (date) {
+      return moment(date).format("YYYY.MM.DD HH:mm:ss");
     },
   },
-}
+};
 </script>
